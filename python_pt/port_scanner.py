45bd1afc571ff2b407bfca6dc183c9717bc85707
jivoi <don.eugen@gmail.com>
2015-08-21 18:57:21 +0300

    python pt

--- python_pt/port_scanner.py
+++ python_pt/port_scanner.py
@@ -16,20 +16,19 @@ s = socket.socket()
 #         s.connect(('192.168.56.101', port))
 #         banner = s.recv(1024)
 #         if banner:
-   print "[+] Port "+str(port)+" open: "+banner
+#             print "[+] Port "+str(port)+" open: "+banner
 #         s.close()
 #     except: pass
 
 hosts = ['127.0.0.1', '192.168.56.101', '10.0.0.1']
 ports = [22, 445, 80, 443, 3389]
 for host in hosts:
-for port in ports:
-try:
-print "[+] Connecting to "+host+":"+str(port)
-s.connect((host, port))
-s.send('Primal Security \n')
-banner = s.recv(1024)
-if banner:
-print "[+] Port "+str(port)+" open: "+banner
-s.close()
-except:pass
+    for port in ports:
+        try:
+            print "[+] Connecting to "+host+":"+str(port)
+            s.connect((host, port))
+            banner = s.recv(1024)
+            if banner:
+                print "[+] Port "+str(port)+" open: "+banner
+            s.close()
+        except:pass

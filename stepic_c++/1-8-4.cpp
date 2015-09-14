4d768295ec4c89608fa71b234d326126d6bcdeeb
jivoi <don.eugen@gmail.com>
2015-09-14 22:47:58 +0300

    c++ junk

--- stepic_c++/1-8-4.cpp
+++ stepic_c++/1-8-4.cpp
@@ -11,19 +11,19 @@ int power(int x, unsigned p) {
 
 int  main () {
 // power(5,5);
-printf ("%g",power(3,5));
-printf ("%g",sqr(3 + 0));
+printf ("%s",power(3,5));
+printf ("%s",sqr(3 + 0));
 }
 
-http://www.programmersforum.ru/showthread.php?t=123333
-http://www.cyberforum.ru/cpp-beginners/thread300222.html
+//http://www.programmersforum.ru/showthread.php?t=123333
+//http://www.cyberforum.ru/cpp-beginners/thread300222.html
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
-#define max(a, b) (((a) > (b)) ? (r)=(a) : (r)=(b))
+#define max(a, b, c) (((a) > (b)) ? (r)=(a) : (r)=(b))
 
-#define MAX(x, y, r) (((x) > (y)) ? (r)=(x) : (r)=(y))
+//#define MAX(x, y, r) (((x) > (y)) ? (r)=(x) : (r)=(y))
 
 int a = 10;
 int b = 20;
 int c = 0;
-max(a, b, c);
+printf(("%s",max(a, b, c)));

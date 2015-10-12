453c2ff2557e6addf7fdaf4464ba3095c755ce46
jivoi <don.eugen@gmail.com>
2015-10-12 19:29:46 +0300

    junk

---
+++ lippman_enter_c++/cpp1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0, val = 1;
+    while (val <= 10) {
+        sum += val;
+        ++val;
+    }
+    std::cout << "Sum of 1 to 10 is: " sum << std::endl;
+    return 0;
+}

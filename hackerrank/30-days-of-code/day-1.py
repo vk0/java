b40d1623c6870769c096cebd4aca8fb5a9e3e44c
jivoi <don.eugen@gmail.com>
2016-04-08 18:28:12 +0300

    step

---
+++ hackerrank/30-days-of-code/day-1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Objective
+# In this challenge, we review some basic concepts that will get you started with this series. Check out the Tutorial tab for learning materials and an instructional video!
+
+# Task
+# To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+# You've got this!
+
+# Note: The instructions are Java-based, but we support submissions in many popular languages. You can switch languages using the drop-down menu above your editor.
+
+# Input Format
+
+# A single line of text denoting inputStringinputString (the variable whose contents must be printed).
+
+# Output Format
+
+# Print Hello, World.Hello, World. on the first line, and the contents of inputStringinputString on the second line.
+
+# Sample Input
+
+# Welcome to 30 Days of Code!
+# Sample Output
+
+# Hello, World.
+# Welcome to 30 Days of Code!
+
+inputString = input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print('Hello, World.')
+
+# Write the second line of output
+print(inputString)

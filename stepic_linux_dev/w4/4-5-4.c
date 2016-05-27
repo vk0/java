7f39a12d5c748ed53c15f74c3b32f185aa08de87
jivoi <don.eugen@gmail.com>
2016-05-27 19:46:53 +0300

    hack

--- stepic_linux_dev/w4/4-5-4.c
+++ stepic_linux_dev/w4/4-5-4.c
@@ -18,3 +18,38 @@
 // Вывод
 
 // Программа выводит в стандартный поток вывода числа (каждое число в отдельной строке)
+
+#include <stdio.h>
+#include <string.h>
+#include <linux/limits.h>
+
+int read_proc(int pid) {
+    char fname[255];
+    FILE *input;
+    int ppid;
+    char tcomm[PATH_MAX];
+    char state;
+
+    sprintf(fname, "/proc/%i/stat", pid);
+    input = fopen(fname, "r");
+
+    fscanf(input, "%i %s %c %i", &pid, tcomm, &state, &ppid);
+    fclose(input);
+    return ppid;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2)
+        return -1;
+    int pid, ppid;
+    int init_pid = 1;
+    sscanf(argv[1], "%i", &pid);
+    printf("%i\n", pid);
+    ppid = read_proc(pid);
+    while (ppid != init_pid) {
+        printf("%i\n", ppid);
+        ppid = read_proc(ppid);
+    }
+    printf("%i\n", init_pid);
+    return 0;
+}

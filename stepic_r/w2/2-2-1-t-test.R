005681c9c2d2fefee33455209c72c79e34d8f8b3
jivoi <don.eugen@gmail.com>
2016-05-29 22:27:40 +0300

    hack

---
+++ stepic_r/w2/2-2-1-t-test.R
@@ -0,0 +1,68 @@
+?iris
+df  <- iris
+
+str(df)
+
+df1  <- subset(df, Species != "setosa")
+str(df1)
+table(df1$Species)
+
+hist(df1$Sepal.Length)
+
+ggplot(df1, aes(x =Sepal.Length ))+
+  geom_histogram(fill = "white", col = "black", binwidth = 0.4)+
+  facet_grid(Species ~ .)
+
+ggplot(df1, aes(Sepal.Length, fill = Species ))+
+  geom_density(alpha = 0.5)
+
+ggplot(df1, aes(Species, Sepal.Length))+
+  geom_boxplot()
+
+shapiro.test(df1$Sepal.Length)
+
+shapiro.test(df1$Sepal.Length[df1$Species == "versicolor"])
+shapiro.test(df1$Sepal.Length[df1$Species == "virginica"])
+
+#The same result in one line
+by(df1$Sepal.Length, INDICES = df1$Species, shapiro.test)
+
+bartlett.test(Sepal.Length  ~ Species, df1)
+
+
+t.test(Sepal.Length  ~ Species, df1)
+test1  <- t.test(Sepal.Length  ~ Species, df1)
+
+str(test1)
+test1$p.value
+
+t.test(Sepal.Length  ~ Species, df1, var.equal = T)
+
+t.test(df1$Sepal.Length, mu = 8)
+
+t.test(df1$Petal.Length, df1$Petal.Width, paired = T)
+
+ggplot(df1, aes(Species, Sepal.Length))+
+  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", 
+               width = 0.1)+
+  stat_summary(fun.y = mean, geom = "point", size = 4)
+
+ggplot(df1, aes(Species, Sepal.Length))+
+  stat_summary(fun.data = mean_cl_normal, geom = "pointrange", 
+               size = 2)
+
+
+?wilcox.test
+
+test2  <- wilcox.test(Petal.Length ~ Species, df1)
+pv  <- test2$p.value
+
+ggplot(df1, aes(Species, Petal.Length))+
+  geom_boxplot()
+
+
+wilcox.test(df1$Petal.Length, df1$Petal.Width, paired = T)
+
+paired_wtest  <- wilcox.test(df1$Petal.Length, df1$Petal.Width, paired = T)
+
+paired_wtest$p.value

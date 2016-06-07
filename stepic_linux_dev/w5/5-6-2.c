07468ca06444a63613ec9d48b78ecbe07d241217
jivoi <don.eugen@gmail.com>
2016-06-07 19:22:38 +0300

    hacK

--- stepic_linux_dev/w5/5-6-2.c
+++ stepic_linux_dev/w5/5-6-2.c
@@ -21,65 +21,66 @@
 #include <stdio.h>
 #include <sys/select.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int main() {
-    int fd1, fd2;
-    int total = 0;
-    char buf1[256];
-    char buf2[256];
+int main(void) {
+  int fds[2];
+  char buf[4096];
+  int i, rc, maxfd;
+  int total = 0;
+  fd_set watchset; /* fds для чтения */
+  // fd_set tmp_set;    /* обновляется select() */
 
-    /* open both pipes */
-    if ((fd1 = open("in1", O_RDONLY | O_NONBLOCK)) < 0) {
-        perror("open p1");
-        return 1;
+  /* открыть оба канала */
+  if ((fds[0] = open("in1", O_RDONLY | O_NONBLOCK)) < 0) {
+    perror("open in1");
+    return 1;
+  }
+
+  if ((fds[1] = open("in2", O_RDONLY | O_NONBLOCK)) < 0) {
+    perror("open in2");
+    return 1;
+  }
+
+  /* начать чтение из обоих файловых дескрипторов */
+  FD_ZERO(&watchset);
+  FD_SET(fds[0], &watchset);
+  FD_SET(fds[1], &watchset);
+
+  /* найти максимальный файловый дескриптор */
+  maxfd = fds[0] > fds[1] ? fds[0] : fds[1];
+
+  /* пока наблюдаем за одним из fds[0] или fds[1] */
+  while (FD_ISSET(fds[0], &watchset) || FD_ISSET(fds[1], &watchset)) {
+    /* здесь копируем watchset, потому что select() обновляет его */
+    fd_set inset = watchset;
+    if (select(maxfd + 1, &inset, NULL, NULL, NULL) < 0) {
+      perror("select");
+      return 1;
     }
 
-    if ((fd2 = open("in2", O_RDONLY | O_NONBLOCK)) < 0) {
-        perror("open p2");
-        return 1;
+    /* проверить, какой из файловых дескрипторов
+       готов для чтения из него */
+    for (i = 0; i < 2; i++) {
+      if (FD_ISSET(fds[i], &inset)) {
+        /* fds[i] готов для чтения, двигаться дальше... */
+        rc = read(fds[i], buf, sizeof(buf) - 1);
+        if (rc < 0) {
+          perror("read");
+          return 1;
+        } else if (!rc) {
+          /* этот канал закрыт, не пытаться
+             читать из него снова */
+          close(fds[i]);
+          FD_CLR(fds[i], &watchset);
+        } else {
+          buf[rc] = '\0';
+          total = total + strtoul(buf, NULL, 10);
+          // printf("чтение: %s", buf);
+        }
+      }
     }
-
-    while (1) {
-        int sum = 2;
-        int rc, maxfd;
-        fd_set watchset;       /* fds to read from */
-        fd_set inset;          /* updated by select() */
-
-        FD_ZERO(&watchset);
-        FD_SET(fd1, &watchset);
-        FD_SET(fd2, &watchset);
-
-        maxfd = fd1 > fd2 ? fd1 : fd2;
-        inset = watchset;
-
-        if (select(maxfd + 1, &inset, NULL, NULL, NULL) < 0) {
-            perror("select");
-            return 1;
-        }
-
-        if (FD_ISSET(fd1, &inset)) {
-            rc = read(fd1, buf1, sizeof(buf1));
-            total = total + atoi(buf1);
-            if (rc < 0) {
-                perror("read");
-                return 1;
-            } else {
-                sum--;
-            }
-        }
-        if (FD_ISSET(fd2, &inset)) {
-            rc = read(fd2, buf2, sizeof(buf2));
-            total = total + atoi(buf1);
-            if (rc < 0) {
-                perror("read");
-                return 1;
-            } else {
-                sum--;
-            }
-        }
-        if (sum == 0)
-            break;
-    }
-    printf("%d\n", total);
-    return 0;
+  }
+  printf("%d\n", total);
+  return 0;
 }
--- stepic_linux_dev/w5/5-6-5.c
+++ stepic_linux_dev/w5/5-6-5.c
@@ -15,3 +15,74 @@
 // Вывод
 
 // Программа выводит в стандартный поток вывода ключ созданного региона памяти (Строка завершается символом конца строки)
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SIZE_SM 1000 /* make it a 1K shared memory segment */
+#define COUNT 100
+
+int main(int argc, char *argv[])
+{
+    key_t key_1 = atoi(argv[1]);
+    key_t key_2 = atoi(argv[2]);
+    key_t key = ftok("solution.c", 'a'); /*Here the file must exist */
+
+    if (key == -1) {
+        printf("ftok error\n");
+    }
+
+    // fprintf(stderr, "%d\n", key);
+    // fprintf(stderr, "%d\n", key_1);
+    // fprintf(stderr, "%d\n", key_2);
+
+    /*  create the segment: */
+    int shmid = shmget(key, SIZE_SM, IPC_CREAT | 0666);
+    int *arr = (int*)shmat(shmid, 0, 0);
+
+    /* attach to the segment to get a pointer to it: */
+    int shmid_1 = shmget(key_1, SIZE_SM, 0666);
+    int *arr_shmaddr_1 = (int*)shmat(shmid_1, 0, 0);
+    int shmid_2 = shmget(key_2, SIZE_SM, 0666);
+    int *arr_shmaddr_2 = (int*)shmat(shmid_2, 0, 0);
+
+    // fprintf(stderr, "%d\n", shmid);
+    // fprintf(stderr, "%d\n", shmid_1);
+    // fprintf(stderr, "%d\n", shmid_2);
+
+    memset(arr, 0, SIZE_SM);
+
+    if (arr == (int*)(-1)) {
+        fprintf(stderr, "err0\n");
+    }
+    if (arr_shmaddr_1 == (int*)(-1)) {
+        fprintf(stderr, "err1\n");
+    }
+    if (arr_shmaddr_2 == (int*)(-1)) {
+        fprintf(stderr, "err2\n");
+    }
+
+    int i;
+    for (i = 0; i < COUNT; i++) {
+        arr[i] = arr_shmaddr_1[i] + arr_shmaddr_2[i];
+        // fprintf(stderr, "%d=%d+%d:\n", arr[i], arr_shmaddr_1[i], arr_shmaddr_2[i]);
+    }
+
+    /* detach from the segment: */
+    if (shmdt(arr) == -1) {
+        fprintf(stderr, "err5\n");
+    }
+    if (shmdt(arr_shmaddr_1) == -1) {
+        fprintf(stderr, "err6\n");
+    }
+    if (shmdt(arr_shmaddr_2) == -1) {
+        fprintf(stderr, "err7\n");
+    }
+    printf("%d\n", key);
+    return 0;
+}

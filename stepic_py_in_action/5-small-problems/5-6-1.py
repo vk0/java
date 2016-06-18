ea1b9ccf649e02c19643ce4171a3d108cd65a941
jivoi <don.eugen@gmail.com>
2016-06-18 18:24:16 +0300

    hack

---
+++ stepic_py_in_action/5-small-problems/5-6-1.py
@@ -0,0 +1,35 @@
+# Не используя интерпретатор, отметьте все однозначно неправильные утверждения о содержимом переменных после выполнения этого кода:
+
+# dict1 = dict(zip(('a', 'b', 'c'), (1, 2, 3)))
+# range5 = list(range(5))
+# lst1 = [i
+#     for i in range5
+#     if i in dict1]
+# dict2 = {i: i*i for i in range5}
+# lst2 = [dict1[value] for value in dict1]
+# lst3 = [i
+#     for i in range5
+#     if i in lst2]
+
+# print("dict1", dict1)
+# print("lst1", lst1)
+# print("dict2", dict2)
+# print("lst2", lst2)
+# print("lst3", lst3)
+# print("range5", range5)
+
+dict2: {}
+dict2: {0: 'i*i', 1: 'i*i', 2: 'i*i', 3: 'i*i', 4: 'i*i'}
+lst3: []
+dict1: {'a': 1, 'c': 3, 'b': 2}
+range5: [1, 2, 3, 4, 5]
+lst1: [1, 2, 3]
+lst2: ['a', 'b', 'c']
+lst3: [1, 3, 2]
+lst1: []
+dict2: {0: 'i*i', 1: 'i*i', 2: 'i*i', 3: 'i*i', 4: 'i*i', 5: 'i*i'}
+range5: [1, 2, 3, 4]
+lst3: ['a', 'b', 'c']
+range5: [0, 1, 2, 3, 4, 5]
+lst2: []
+dict2: {1: 'i*i', 2: 'i*i', 3: 'i*i', 4: 'i*i', 5: 'i*i'}

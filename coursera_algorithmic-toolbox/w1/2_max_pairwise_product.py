665af82086d9f0ebef21d9dc749310f8a024146d
jivoi <ekoz@protonmail.com>
2016-07-29 19:05:55 +0300

    step

--- coursera_algorithmic-toolbox/w1/2_max_pairwise_product.py
+++ coursera_algorithmic-toolbox/w1/2_max_pairwise_product.py
@@ -34,15 +34,30 @@
 # Explanation:
 # 140=14×10
 
+def MaxPairwiseProductSlow(a,n):
+    result = 0
+    for i in range(0, n):
+        for j in range(i+1, n):
+            if a[i]*a[j] > result:
+                result = a[i]*a[j]
+    return result
+
+def MaxPairwiseProductFast(a,n):
+    max_index1 = -1
+    max_index2 = -1
+    for i in range(0,n):
+        if max_index1 == -1 or a[i] > a[max_index1]:
+            max_index1 = i
+
+    for i in range(0,n):
+        if (max_index2 == -1 and max_index1 != i) or (a[i] > a[max_index2] and max_index1 != i):
+            max_index2 = i
+    return a[max_index1]*a[max_index2]
+
+
 n = int(input())
 a = [int(x) for x in input().split()]
 assert(len(a) == n)
 
-result = 0
-
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
-
-print(result)
+# print(MaxPairwiseProductSlow(a,n))
+print(MaxPairwiseProductFast(a,n))
